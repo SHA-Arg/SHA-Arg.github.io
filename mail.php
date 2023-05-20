a32c2f1c6175ee8ddb6b07a3a91499eaeca8ae9f
Seba HA <hereniu.amaral.sebastian@gmail.com>
2023-05-20 11:53:13 -0300

    Cambios

---
+++ mail.php
@@ -0,0 +1,36 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    header('Location: index.html');
+}
+
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/Exception.php';
+
+
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+
+if (empty(trim($nombre))) $nombre = "Anónimo";
+if (empty(trim($asunto))) $asunto = "Sin asunto";
+
+$body = <<< HTML
+<h1>Consulta desde la Web</h1>
+<p>De: $nombre / $email</p>
+<h1>Mensaje:</h1>
+$mensaje
+HTML;
+
+$mailer = new PHPMailer();
+$mailer->setFrom($email, "$nombre");
+$mailer->addAddress('herniu.amaral.sebastian@gmail.com', 'SHA Dev.');
+$mailer->msgHTML($body);
+$mailer->AltBody = strip_tags($body);
+$rta = $mailer->send();
+
+var_dump($rta);
+
+header('Location: projecto.html');
