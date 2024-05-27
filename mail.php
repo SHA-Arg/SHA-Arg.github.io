9350c8a57185f5f1b4f6741bc4cb450241a5769f
Sebastian Amaral <56357599+SHA-Arg@users.noreply.github.com>
2024-05-27 19:37:53 -0300

    prueba de mail con php

--- mail.php
+++ mail.php
@@ -1,4 +1,19 @@
 <?php
+
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    header('Location: index.html');
+}
+
+
 $nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+
+// Remitente del mensaje (nombre y email)
+// Asunto del mensaje
+// Cuerpo o contenido del mensaje
+
 var_dump($nombre);
-printf("Hola %s", $nombre);
+$respuesta = mail('se_hereu@yahoo.com.ar', "Mensaje de Portfolio: $nombre", $mensaje, "From: $email");
+var_dump($respuesta);
